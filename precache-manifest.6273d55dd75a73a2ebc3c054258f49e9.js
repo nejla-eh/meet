56a15a82389620c1b31e3e04b2ca2fac49925be8
Nejla <nejlaefendic@hotmail.com>
2022-10-28 14:32:22 +0200

    Updates

---
+++ precache-manifest.6273d55dd75a73a2ebc3c054258f49e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c01eee2f50675dd7fddc",
+    "url": "/meet/static/css/main.2d4ee7b2.chunk.css"
+  },
+  {
+    "revision": "c01eee2f50675dd7fddc",
+    "url": "/meet/static/js/main.fc437ed9.chunk.js"
+  },
+  {
+    "revision": "1b9c9f39f8be985be7b7",
+    "url": "/meet/static/js/runtime~main.454a05bf.js"
+  },
+  {
+    "revision": "bc9230ae983c416626be",
+    "url": "/meet/static/js/2.b8c8d112.chunk.js"
+  },
+  {
+    "revision": "0d238a3a755f2811deea",
+    "url": "/meet/static/js/3.d90efc57.chunk.js"
+  },
+  {
+    "revision": "66bf3628ddd2ce8b2c11abb6c56ba3de",
+    "url": "/meet/index.html"
+  }
+];
