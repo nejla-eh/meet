c76c55b909eb49284ee6dd841273f47c8e7467a5
Nejla <nejlaefendic@hotmail.com>
2022-11-07 15:49:04 +0100

    Updates

---
+++ precache-manifest.f4bda869eab54488406edc619e5352cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfebf928926c4dd7754b",
+    "url": "/meet/static/css/main.c69278dc.chunk.css"
+  },
+  {
+    "revision": "dfebf928926c4dd7754b",
+    "url": "/meet/static/js/main.9a2346a6.chunk.js"
+  },
+  {
+    "revision": "eca2fd467cc568a52ba4",
+    "url": "/meet/static/js/runtime~main.5caa83e7.js"
+  },
+  {
+    "revision": "7911b54d290dd116a1a2",
+    "url": "/meet/static/js/2.afefc897.chunk.js"
+  },
+  {
+    "revision": "2c6b046a7682591c78d0",
+    "url": "/meet/static/js/3.0dd53fc1.chunk.js"
+  },
+  {
+    "revision": "b0b8a509022800b741cc9fae56b10510",
+    "url": "/meet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meet/precache-manifest.eb7088e4b42ccac5fbf008663696111d.js"
+  "/meet/precache-manifest.f4bda869eab54488406edc619e5352cc.js"
 );
 
 workbox.clientsClaim();
