598350d238722c13204fe71c82589c50ce4c4bd3
Nejla <nejlaefendic@hotmail.com>
2022-11-09 16:26:47 +0100

    fix syntax

--- src/api.js
+++ src/api.js
@@ -69,7 +69,7 @@ export const getEvents = async () => {
     return mockData;
   }
 
-  if (!navigator.online) {
+  if (!navigator.onLine) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
     return data ? JSON.parse(data).events : [];
