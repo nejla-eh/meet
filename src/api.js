acdd717d8fe4132b9cd1099e7528378e09e263f8
Nejla <nejlaefendic@hotmail.com>
2022-11-09 12:44:37 +0100

    fix error

--- src/api.js
+++ src/api.js
@@ -72,7 +72,7 @@ export const getEvents = async () => {
   if (!navigator.online) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    return data ? JSON.parse(events).events : [];
+    return data ? JSON.parse(data).events : [];
   }
 
   const token = await getAccessToken();
